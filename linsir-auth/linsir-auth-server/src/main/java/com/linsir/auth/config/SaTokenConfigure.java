6e8a3c4816da271535f7ec0fdd23ab74ecee695e
linsir <student_yxl@126.com>
2025-02-22 23:07:19 +0800

    oauth2 基础搭建完成

--- linsir-auth/linsir-auth-server/src/main/java/com/linsir/auth/config/SaTokenConfigure.java
+++ linsir-auth/linsir-auth-server/src/main/java/com/linsir/auth/config/SaTokenConfigure.java
@@ -1,10 +1,22 @@
 package com.linsir.auth.config;
 
 
+import cn.dev33.satoken.SaManager;
 import cn.dev33.satoken.config.SaTokenConfig;
+import cn.dev33.satoken.context.SaHolder;
+import cn.dev33.satoken.filter.SaServletFilter;
+import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.dev33.satoken.oauth2.config.SaOAuth2ServerConfig;
+import cn.dev33.satoken.router.SaHttpMethod;
+import cn.dev33.satoken.router.SaRouter;
+import cn.dev33.satoken.stp.StpUtil;
+import cn.dev33.satoken.util.SaResult;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * description：SaTokenConfigure
@@ -13,7 +25,16 @@ import org.springframework.context.annotation.Primary;
  * date       ：2025/2/21 10:41
  */
 @Configuration
-public class SaTokenConfigure {
+public class SaTokenConfigure implements WebMvcConfigurer {
+
+
+
+    public SaTokenConfigure(SaOAuth2ServerConfig saOAuth2ServerConfig) {
+        saOAuth2ServerConfig.doLoginHandle = (name,pwd)->{
+            StpUtil.login("1001");
+            return SaResult.ok();
+        };
+    }
 
     @Bean
     @Primary
@@ -28,5 +49,58 @@ public class SaTokenConfigure {
         config.setIsLog(true);                     // 是否输出操作日志
         config.setJwtSecretKey("linsir"); //配置密钥
         return config;
+    }
+
+    /**
+     * 注册 Sa-Token 拦截器打开注解鉴权功能
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册 Sa-Token 拦截器打开注解鉴权功能
+        registry.addInterceptor(new SaInterceptor()).addPathPatterns("/**");
+    }
+
+    /**
+     * 注册 [Sa-Token 全局过滤器]
+     */
+    @Bean
+    public SaServletFilter getSaServletFilter() {
+        return new SaServletFilter()
+
+                // 指定 [拦截路由] 与 [放行路由]
+                .addInclude("/**").addExclude("/favicon.ico")
+
+                // 认证函数: 每次请求执行
+                .setAuth(obj -> {
+                    SaManager.getLog().debug("----- 请求path={}  提交token={}", SaHolder.getRequest().getRequestPath(), StpUtil.getTokenValue());
+                    // ...
+                })
+
+                // 异常处理函数：每次认证函数发生异常时执行此函数
+                .setError(e -> {
+                    return SaResult.error(e.getMessage());
+                })
+
+                // 前置函数：在每次认证函数之前执行
+                .setBeforeAuth(obj -> {
+                    SaHolder.getResponse()
+
+                            // ---------- 设置跨域响应头 ----------
+                            // 允许指定域访问跨域资源
+                            .setHeader("Access-Control-Allow-Origin", "*")
+                            // 允许所有请求方式
+                            .setHeader("Access-Control-Allow-Methods", "*")
+                            // 允许的header参数
+                            .setHeader("Access-Control-Allow-Headers", "*")
+                            // 有效时间
+                            .setHeader("Access-Control-Max-Age", "3600")
+                    ;
+
+                    // 如果是预检请求，则立即返回到前端
+                    SaRouter.match(SaHttpMethod.OPTIONS)
+                            .free(r -> System.out.println("--------OPTIONS预检请求，不做处理"))
+                            .back();
+                })
+                ;
     }
 }
